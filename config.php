fcddb45ce4ae7af5fb65555cf80e7e0605d6e4fc
aejontargaryen <aejontargaryen@protonmail.com>
2021-05-07 01:01:27 +0000

    more updates & add paperwallet

--- config.php
+++ config.php
@@ -1,8 +1,6 @@
 <?php
 return array(
-        'api' => '127.0.0.1:18180',
-        'pool' => 'https://explorer.conceal.network/pool',
-        'services' => 'https://explorer.conceal.network/services',
+        'api' => 'http://104.236.0.16:18180',
         'blockTargetInterval' => 480,
         'coinUnits' => 10000000
 );
--- js/crypto_utils.js
+++ js/crypto_utils.js
@@ -1624,11 +1624,11 @@ return{_strlen:lb,_ge_mul8:Va,_keccak:db,_ge_scalarmult:Ta,_ge_fromfe_frombytes_
 
 var salt = '4721283fee2db41e1f2fc50ea9f6d783'; // IMPORTANT: never change it, otherwise it will be impossible to restore previously generated wallets!
 var config = {
-    coinUnitPlaces: 12,
-    coinSymbol: 'TRTL',
-    coinName: 'TurtleCoin',
-    coinUriPrefix: 'turtlecoin:',
-    addressPrefix: 3914525
+    coinUnitPlaces: 7,
+    coinSymbol: 'XFG',
+    coinName: 'Fango',
+    coinUriPrefix: 'fango:',
+    addressPrefix: 203478502
 };
 var cnUtil = (function(initConfig) {
     //var config = $.extend({}, initConfig);
